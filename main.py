01c3ac776a72f50b9e5744905c5d0ce18b17aead
Sai Surya Teja <saisuryateja3@gmail.com>
2026-07-18 19:22:12 +0530

    Pydantic Schemas - Request and Response Validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates 
 from fastapi.staticfiles import StaticFiles
+from schemas import PostCreate, PostResponse
 
 app= FastAPI()
 
@@ -87,19 +88,34 @@ def validation_exception_handler(request: Request, exception: RequestValidationE
         status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
     )
 
-
-@app.get("/api/posts")
+## API Section -------------------------
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
 
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_posts(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+@app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post (post:PostCreate):
+    new_id= max(p["id"] for p in posts) +1 if posts else 1
+
+    new_post={
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content":post.content,
+        "date_posted":"July 18, 2026"
+    }
+
+    posts.append(new_post)
+    return new_post
 
 
 
